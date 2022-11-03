a53a7f433e46ba7844ad11f184af3ed7a4c41dc0
first <first@DESKTOP-8ERI5IS>
2022-11-03 19:25:01 +0200

    Добавьте файлы проекта.

---
+++ assignment8_MaksimTomilin/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace assignment8_MaksimTomilin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BoyNameListbox = new System.Windows.Forms.ListBox();
+            this.GirlNameListbox = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Search name";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(219, 205);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Search name";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(32, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Boy names";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(243, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Girl names";
+            // 
+            // BoyNameListbox
+            // 
+            this.BoyNameListbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BoyNameListbox.FormattingEnabled = true;
+            this.BoyNameListbox.ItemHeight = 18;
+            this.BoyNameListbox.Location = new System.Drawing.Point(12, 25);
+            this.BoyNameListbox.Name = "BoyNameListbox";
+            this.BoyNameListbox.Size = new System.Drawing.Size(120, 148);
+            this.BoyNameListbox.TabIndex = 4;
+            // 
+            // GirlNameListbox
+            // 
+            this.GirlNameListbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GirlNameListbox.FormattingEnabled = true;
+            this.GirlNameListbox.ItemHeight = 18;
+            this.GirlNameListbox.Location = new System.Drawing.Point(219, 25);
+            this.GirlNameListbox.Name = "GirlNameListbox";
+            this.GirlNameListbox.Size = new System.Drawing.Size(120, 148);
+            this.GirlNameListbox.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 179);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(219, 179);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(138, 25);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 148);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 235);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.GirlNameListbox);
+            this.Controls.Add(this.BoyNameListbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Name Search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox BoyNameListbox;
+        private System.Windows.Forms.ListBox GirlNameListbox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ assignment8_MaksimTomilin/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment8_MaksimTomilin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        
+  
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string string1;
+            StreamReader inputFile = File.OpenText("BoyNames.txt");
+            while ((string1 = inputFile.ReadLine()) != null)
+                BoyNameListbox.Items.Add(string1);
+            inputFile.Close();
+
+            string string2;
+            StreamReader inputFile1 = File.OpenText("GirlNames.txt");
+            while ((string2 = inputFile1.ReadLine()) != null)
+                GirlNameListbox.Items.Add(string2);
+            inputFile1.Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Boy = textBox1.Text;
+            bool found = false;
+            for (int n = 0; n < BoyNameListbox.Items.Count; n++)
+            {
+                if ((string)BoyNameListbox.Items[n] == Boy)
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found)
+                MessageBox.Show("Popular name");
+            else
+                MessageBox.Show("Not popular name");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string Girl = textBox2.Text;
+            bool found = false;
+            for (int n = 0; n < GirlNameListbox.Items.Count; n++)
+            {
+                if ((string)GirlNameListbox.Items[n] == Girl)
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found)
+                MessageBox.Show("Popular name");
+            else
+                MessageBox.Show("Not popular name");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
